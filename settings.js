c6349a1323895ef4183938a8c0def655e63dcdd2
vihangasulakshana7806 <jayantha.maytree5@icloud.com>
2025-01-17 18:30:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,8 +8,8 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '263717750825'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
+global.ownernumber = '94729645071'  //creator number
+global.ownername = 'vihanga sulakshana🤣🤣🤔' //owner name
 global.botname = 'PREXZY ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
